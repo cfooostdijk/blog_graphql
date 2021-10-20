83cee74a0d6d21c946e0982d9e5a3ec95f80091f
Cristian <co@qubikdigital.com>
2021-10-20 11:36:18 -0300

    Ok

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,7 +1,13 @@
 Types::PostType = GraphQL::ObjectType.define do
   name 'PostType'
   description 'Represents a blog post in the system'
+
   field :id, !types.ID, 'The ID of the blog post'
   field :title, types.String, 'A user-friendly title for the blog post'
   field :body, types.String, 'The main body of content for the blog post'
- end
+  field :shortBody, types.String, 'A shortened version of the body' do
+    resolve ->(obj, args, ctx) {
+      obj.body[0, 5]
+    }
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,6 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
+
   field :post, Types::PostType do
     description 'Retrieve a blog post by id'
     argument :id, !types.ID, 'The ID of the blog post to retrieve'
@@ -7,4 +8,12 @@ Types::QueryType = GraphQL::ObjectType.define do
       Post.find(args[:id])
     }
   end
- end
+
+  field :posts, types[Types::PostType] do
+    description 'Retrieve a list of all blog posts'
+    resolve ->(obj, args, ctx) {
+      Post.all
+    }
+  end
+  
+end
